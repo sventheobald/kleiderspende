c780b1553b97d727d51b07f1e4325c806fdd0833
Sven Theobald <sven@MacBook-Air-von-Sven.local>
2025-10-29 17:38:07 +0100

    App Struktur aufgebaut mit Komponenten + Pages und Router

--- kleiderspende/src/App.jsx
+++ kleiderspende/src/App.jsx
@@ -1,3 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import RegisterDonation from './pages/RegisterDonation';
+import AboutUs from './pages/AboutUs';
+import Impressum from './pages/Impressum';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
